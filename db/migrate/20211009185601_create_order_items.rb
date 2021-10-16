5d1471b42c125e4019f02d2ac66bbd28b6f6b847
Ignacio Moran <moran.ua@gmail.com>
2021-10-16 00:39:17 -0300

    database changed

---
+++ db/migrate/20211009185601_create_order_items.rb
@@ -0,0 +1,13 @@
+class CreateOrderItems < ActiveRecord::Migration[5.2]
+    def change
+      create_table :order_items do |t|
+        t.integer :quantity
+        t.decimal :total
+        t.decimal :unit_price
+        t.references :product, foreign_key: true
+        t.references :order, foreign_key: true
+  
+        t.timestamps
+      end
+    end
+  end
