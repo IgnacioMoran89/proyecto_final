41c9d475ddb1b7ae37548535da488853800461ad
Ignacio Moran <moran.ua@gmail.com>
2021-10-23 21:40:02 -0300

    befero merge master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://flormarketapp.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 end
