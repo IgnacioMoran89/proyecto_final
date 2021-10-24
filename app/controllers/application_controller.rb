1e1ce0f2f9bd9b740d5bcc0384e4fb562362588d
Ignacio Moran <moran.ua@gmail.com>
2021-10-23 22:19:36 -0300

    mailer added to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
 
         :address            => 'smtp.gmail.com',
         :port               => 587,
-        :domain             => 'mail.google.com', #you can also use google.com
-        :authentication     => 'login',
+        :domain             => 'https://flormarketapp.herokuapp.com/', #you can also use google.com
+        :authentication     => 'plain',
         :user_name          => ENV['GMAIL_USERNAME'],
         :password           => ENV['GMAIL_PASSWORD'],
         :enable_starttls_auto => true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://flormarketapp.herokuapp.com/' }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: "utf-8"
   config.action_mailer.delivery_method = :smtp
 end
