e29b18403d3e50d5d33bb8989aab8723148ce682
Ignacio Moran <moran.ua@gmail.com>
2021-11-01 19:46:31 -0300

    smtp trying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.delivery_method = :smtp
+  
   ActionMailer::Base.smtp_settings = {
 
     :address            => 'smtp.gmail.com',
