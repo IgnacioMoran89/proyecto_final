8d50fda6e13f355f76ae34fdea766dfa26635ae6
Ignacio Moran <moran.ua@gmail.com>
2021-11-01 20:00:23 -0300

    product new fixed

--- config/application.rb
+++ config/application.rb
@@ -17,4 +17,15 @@ module MarketplaceApp
     # the framework and any gems in your application.
   end
 
+  ActionMailer::Base.smtp_settings = {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'https://flormarketapp.herokuapp.com/', #you can also use google.com
+    :authentication     => 'plain',
+    :user_name          => ENV['GMAIL_USERNAME'],
+    :password           => ENV['GMAIL_PASSWORD'],
+    :enable_starttls_auto => true
+     
+}
 end
